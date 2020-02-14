071b98d74d601bc8a6864e6300753c5c9e6c3a81
Karenkoutsuki <60053375+Karenkoutsuki@users.noreply.github.com>
2020-02-14 11:59:23 -0800

    Update and rename CS6250_p3_switch.py to CS6250_p2_switch.py

--- CS6250_p2_switch.py
+++ CS6250_p2_switch.py
@@ -1,83 +1,40 @@
 from Message import *
-
 from StpSwitch import *
-
-
-
 
 
 class Switch(StpSwitch):
 
-
-
-
     def __init__(self, idNum, topolink, neighbors):
-
         super(Switch, self).__init__(idNum, topolink, neighbors)
-
         self.root = self.switchID
-
         self.distance = 0
-
         self.switch_through = self.switchID
-
         # Records if a switch needs to go through me to reach the root.
-
         self.through_me = {}
 
-
-
-
     def send_initial_messages(self):
-
         for destination in self.links:
-
             self.send_message(
-
                 Message(self.root, self.distance, self.switchID, destination, False))
-
         return
-
-
-
 
     def process_message(self, message):
-
         self.through_me[message.origin] = message.pathThrough
-
         if (
-
             (message.root < self.root) or
-
             (message.root == self.root and message.distance + 1 < self.distance) or
-
             (message.root == self.root and message.distance + 1 ==
-
              self.distance and message.origin < self.switch_through)
-
         ):
-
             self.root = message.root
-
             self.distance = message.distance + 1
-
             self.switch_through = message.origin
-
             for destination in self.links:
-
                 self.send_message(Message(self.root, self.distance,
-
                                           self.switchID, destination, destination == self.switch_through))
-
         return
 
-
-
-
     def generate_logstring(self):
-
         answers = filter(
-
             lambda link: self.through_me[link] or link == self.switch_through, self.links)
-
         return ', '.join(["{} - {}".format(self.switchID, neighbor) for neighbor in sorted(answers)])
